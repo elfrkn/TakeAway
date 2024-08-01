a7e55940032bd62a420c571722f2eda91eb873fc
Elif ÇALIŞ <pcengelf@hotmail.com>
2024-08-01 14:24:50 +0200

    Jwt

--- Frontends/TakeAway.WebUI/Program.cs
+++ Frontends/TakeAway.WebUI/Program.cs
@@ -1,6 +1,12 @@
+using TakeAway.WebUI.Services.ProductServices;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddHttpClient();
+builder.Services.AddScoped<IProductService, ProductService>();
+
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
