bfe93d970becf42561e101304a927eb301ff641b
Elif ÇALIŞ <pcengelf@hotmail.com>
2024-08-08 14:29:51 +0200

    SignalR

---
+++ TakeAwaySignalR.Api/DAL/Context.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TakeAwaySignalR.Api.DAL
+{
+    public class Context :DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=MSI;database=TakeAwaySignalRDb;integrated security=true;");
+        }
+        public  DbSet<Delivery> Deliveries { get; set; }
+    }
+}
